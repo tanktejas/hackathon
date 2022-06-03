087fde3fc23955cb70376a5a16eed87f11e54a4e
tejas-tank <tejasget2learn@gmail.com>
2022-06-03 15:13:10 +0530

    add

--- src/Components/footer/footer.js
+++ src/Components/footer/footer.js
@@ -1,131 +1,249 @@
-import React from "react";
+import React, { useState } from "react";
 import "./footer.css";
 
-
 function Footer() {
+  const [arr,setarr]=useState([])
+
   return (
-    <> 
-    <footer className="footer nav-folderized">
-<div className="container ">
-<div className=" c a address-f footer-address-main">
-<div className=" b footer-adress">
+    <>
+      <footer className="footer nav-folderized">
+        <div className="container ">
+          <div className=" c a address-f footer-address-main">
+            <div className=" b footer-adress">
+              <i class="fa fa-map-marker"></i>
 
-           
-            <i class="fa fa-map-marker"></i>
-            
-{/* <h5>India</h5> */}
-<p>01S09 A.M.Naik house of schol, V.V.Nagar <br/>Anand, Gujarat 380010</p>
-</div>
-<div className="b footer-awards">
-<h5>Contact Us</h5>
-<div class="footer-center">
-<div>
-            <i class="fa fa-phone"></i>
-            <p style={{margin:"auto"}}>+91 9309090932</p>
+              {/* <h5>India</h5> */}
+              <p>
+                01S09 A.M.Naik house of schol, V.V.Nagar <br />
+                Anand, Gujarat 380010
+              </p>
+            </div>
+            <div className="b footer-awards">
+              <h5>Contact Us</h5>
+              <div class="footer-center">
+                <div>
+                  <i class="fa fa-phone"></i>
+                  <p style={{ margin: "auto" }}>+91 9309090932</p>
+                </div>
+                <div>
+                  <i class="fa fa-envelope"></i>
+                  <p>
+                    <a href="mailto:support@company.com">Scholar@gmail.com</a>
+                  </p>
+                </div>
+              </div>
+            </div>
 
+            <div className="b footer-social home-social">
+              <h5>Follow us on</h5>
+
+              <ul class="footer-icons">
+                <li>
+                  <a href="#">
+                    <i class="fa fa-facebook"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-github"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-<div>
-<i class="fa fa-envelope"></i>
+        </div>
+        <div className="col-md-12 col-sm-12">
+          <hr />
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="footer-link ftr__list nav">
+              <h5>Quick Links</h5>
+              <ul>
+                <li>
+                  <a href="aboutus.shtml" className="lear-more-l">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="testimonials.shtml" className="lear-more-l">
+                    Testimonials
+                  </a>
+                </li>
+                <li className="hiring-link blink-menu">
+                  <a href="career.shtml" className="lear-more-l blink-active">
+                    Admin Login
+                  </a>{" "}
+                  <span className="hiring">We Are Hiring</span>
+                </li>
+                <li>
+                  <a href="#" className="lear-more-l">
+                    About Us
+                  </a>
+                </li>
+                <li>
+                  <a href="faqs.shtml" className="lear-more-l">
+                    Service
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="footer-link ftr__list nav">
+              <h5>National Scholarships</h5>
+              <ul>
+                <li>
+                  <a href="#" className="lear-more-l">
+                    State Scholarships
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="lear-more-l">
+                    Central Scholarships
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="lear-more-l">
+                    Private Scholarship
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="lear-more-l">
+                    Compititive Exam <br /> based Scholarships
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="footer-link ftr__list nav">
+              <h5>international Scholarships</h5>
+              <ul>
+                <li>
+                  <a href="#" className="lear-more-l">
+                    Study at Jermany
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="lear-more-l">
+                    Study at Us
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="lear-more-l">
+                    Study at Ukraine
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="lear-more-l">
+                    Study at Canada
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="lear-more-l">
+                    Study at Rusia
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="footer-link ftr__list nav">
+              <h5>Research Fellowships</h5>
+              <ul>
+                <li>
+                  <a href="#" className="lear-more-l">
+                    PMRF Fellowship
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="lear-more-l">
+                    TARE Fellowship
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="lear-more-l">
+                    JNFF Fellowship
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="lear-more-l">
+                    Google Phd Fellowship
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="lear-more-l">
+                    ICMR Fellowship
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="footer-link ftr__list nav">
+              <h5>Exams</h5>
+              <ul>
+                <li>
+                  <a href="#" className="lear-more-l">
+                    NDA
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="lear-more-l">
+                    SSB
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="lear-more-l">
+                    GATE
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="lear-more-l">
+                    KVPY
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="lear-more-l">
+                    JEE
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-md-12 col-sm-12 col-xs-12 copy-right text-center">
             <p>
-              <a href="mailto:support@company.com">Scholar@gmail.com</a>
+              <a
+                href="https://www.dmca.com/Protection/Status.aspx?ID=b77fb9ca-2075-44ae-9d84-a87cbe917c18&amp;refurl=#9"
+                title="DMCA.com Protection Status"
+                className="dmca-badge"
+              >
+                <img
+                  src="https://images.dmca.com/Badges/dmca-badge-w100-5x1-03.png?ID=b77fb9ca-2075-44ae-9d84-a87cbe917c18"
+                  alt="DMCA.com Protection Status"
+                  width="100"
+                  height="20"
+                />
+              </a>
+              <span>
+                © 2022
+                <a href="#" target="_new">
+                  {" "}
+                  Scholar{" "}
+                </a>
+                All rights reserved.
+              </span>
             </p>
-</div>
-</div>
+          </div>
+        </div>
+      </footer>
 
-</div>
-
-<div className="b footer-social home-social">
-<h5>Follow us on</h5>
-
-<ul class="footer-icons">
-<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-<li><a href="#"><i class="fa fa-github"></i></a></li>
-</ul>
-</div>
-</div>
-</div>
-<div className="col-md-12 col-sm-12">
-<hr/>
-</div>
-<div className="container">
-<div className="row">
-<div className="footer-link ftr__list nav">
-<h5>Quick Links</h5>
-<ul>
-<li><a href="aboutus.shtml" className="lear-more-l">Home</a></li>
-<li><a href="testimonials.shtml" className="lear-more-l">Testimonials</a></li>
-<li className="hiring-link blink-menu"><a href="career.shtml" className="lear-more-l blink-active">Admin Login</a> <span className="hiring">We Are Hiring</span></li>
-<li><a href="#" className="lear-more-l">About Us</a></li>
-<li><a href="faqs.shtml" className="lear-more-l">Service</a></li>
-</ul>
-</div>
-<div className="footer-link ftr__list nav">
-<h5>National Scholarships</h5>
-<ul>
-<li><a href="#" className="lear-more-l">State Scholarships</a></li>
-<li><a href="#" className="lear-more-l">Central Scholarships</a></li>
-<li><a href="#" className="lear-more-l">Private Scholarship</a></li>
-<li><a href="#" className="lear-more-l">Compititive Exam <br/> based Scholarships</a></li>
-</ul>
-
-</div>
-<div className="footer-link ftr__list nav">
-<h5>international Scholarships</h5>
-<ul>
-<li><a href="#" className="lear-more-l">Study at Jermany</a></li>
-<li><a href="#" className="lear-more-l">Study at Us</a></li>
-<li><a href="#" className="lear-more-l">Study at Ukraine</a></li>
-<li><a href="#" className="lear-more-l">Study at Canada</a></li>
-<li><a href="#" className="lear-more-l">Study at Rusia</a></li>
-</ul>
-
-</div>
-<div className="footer-link ftr__list nav">
-<h5>Research Fellowships</h5>
-<ul>
-<li><a href="#" className="lear-more-l">PMRF Fellowship</a></li>
-<li><a href="#" className="lear-more-l">TARE Fellowship</a></li>
-<li><a href="#" className="lear-more-l">JNFF Fellowship</a></li>
-<li><a href="#" className="lear-more-l">Google Phd Fellowship</a></li>
-<li><a href="#" className="lear-more-l">ICMR Fellowship</a></li>
-</ul>
-
-</div>
-<div className="footer-link ftr__list nav">
-<h5>Exams</h5>
-<ul>
-<li><a href="#" className="lear-more-l">NDA</a></li>
-<li><a href="#" className="lear-more-l">SSB</a></li>
-<li><a href="#" className="lear-more-l">GATE</a></li>
-<li><a href="#" className="lear-more-l">KVPY</a></li>
-<li><a href="#" className="lear-more-l">JEE</a></li>
-</ul>
-
-</div>
-</div>
-<div className="col-md-12 col-sm-12 col-xs-12 copy-right text-center">
-<p>
-<a href="https://www.dmca.com/Protection/Status.aspx?ID=b77fb9ca-2075-44ae-9d84-a87cbe917c18&amp;refurl=#9" title="DMCA.com Protection Status" className="dmca-badge">
- <img src="https://images.dmca.com/Badges/dmca-badge-w100-5x1-03.png?ID=b77fb9ca-2075-44ae-9d84-a87cbe917c18" alt="DMCA.com Protection Status" width="100" height="20"/>
- </a>
-  <span>
-  © 2022 
-  <a href="#" target="_new">
- {" "} Scholar {" "}
- </a>
-  All rights reserved.
-   </span> 
-</p>
-</div>
-
-</div>
-</footer>
-
-<script src="../js/start.js"></script>
-<script src="../js/mmenu-light.js"></script>
-    </> 
+      <script src="../js/start.js"></script>
+      <script src="../js/mmenu-light.js"></script>
+    </>
   );
 }
 
