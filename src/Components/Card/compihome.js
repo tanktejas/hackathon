dfa683ce016c46a68b874d87038be71c0735e843
tejas-tank <tejastank20001@gmail.com>
2022-07-07 10:43:25 +0530

    add

--- src/Components/Card/compihome.js
+++ src/Components/Card/compihome.js
@@ -21,18 +21,66 @@ function Compihome() {
     setsta(false);
     const q = query(collection(db, "competition"));
     onSnapshot(q, (qS) => {
-      let data = qS.docs;
-      setsch(data);
-      setperdata(data);
+      let dataa = qS.docs;
+      setsch(dataa);
+      setperdata(dataa);
       setsta(true);
     });
   }, []);
-  console.log(data);
+
+  const [search, setsearch] = useState("");
+
+  const filter = () => {
+    let tt = [...per];
+    let dd = "";
+    
+    setsch(
+      tt.filter((item) => {
+        console.log(item.data());
+        return item
+          .data()
+          .name.toLocaleLowerCase()
+          .includes(search.toLocaleLowerCase());
+      })
+    );
+    console.log(search);
+    console.log(data);
+  };
+
+  //   console.log(data);
   if (!st) {
     return <h1>Loading...</h1>;
   }
   return (
     <>
+      <section class="bg-primary text-light p-5">
+        <div class="container">
+          <div class="d-md-flex justify-content-around align-items-center">
+            <h3 class="mb-3 mb-md-0 cc">Search Competition</h3>
+
+            <div class="input-group news-input">
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Competition Name"
+                value={search}
+                onChange={(e) => {
+                  setsearch(e.target.value);
+                }}
+              />
+              <button
+                class="btn btn-dark btn-lg"
+                type="button"
+                onClick={() => {
+                  filter();
+                }}
+              >
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
       <div>
         {data.map((item) => {
           return (
